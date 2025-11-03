a188d6ad2abc5adcd9ce44e3c303a315dd868908
anger issues <skuenemy@gmail.com>
2025-11-04 00:40:59 +0300

    update models

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,3 +1,22 @@
 // User, session structs
 
 package auth
+
+import "time"
+
+type UserCredential struct { 
+	ID int `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type RefreshSession struct { 
+	ID int `json:"id"`
+	UserID int `json:"user_id"`
+	Token string `json:"token"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
--- internal/auth/repository.go
+++ internal/auth/repository.go
@@ -1,3 +1,8 @@
 //work for postgres and redis
 
 package auth
+
+
+type CreateCredentialRepository interface { // need to create user in postgres and redis
+	//pass 
+}
